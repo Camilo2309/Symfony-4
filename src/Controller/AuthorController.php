728e6ffd5df378d2e94d8364295c6a112c9cb2e2
Camilo <camilo.bolanos23@hotmail.com>
2018-11-12 12:34:32 +0100

    quete fini enfin !

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: camilo
+ * Date: 12/11/18
+ * Time: 12:03
+ */
+
+namespace App\Controller;
+
+
+class AuthorController
+{
+
+}
---
+++ src/Controller/BlogController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: camilo
+ * Date: 12/11/18
+ * Time: 09:47
+ */
+
+namespace App\Controller;
+
+
+class BlogController
+{
+
+}
